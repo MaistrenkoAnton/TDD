716c351294b47008277c851b1692371438c9f33a
Anton <amaistrenko@s-pro.io>
2019-07-24 13:47:38 +0300

    Added image upload feature

--- app/recipe/tests/test_recipe_api.py
+++ app/recipe/tests/test_recipe_api.py
@@ -234,5 +234,5 @@ class RecipeImageUploadTest(TestCase):
     def test_upload_image_bad_request(self):
         """Test uploading an invalid image"""
         url = image_upload_url(self.recipe.id)
-        res = self.client.post(url, {'image', 'notimage'}, format='multipart')
+        res = self.client.post(url, {'image': 'notimage'}, format='multipart')
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
